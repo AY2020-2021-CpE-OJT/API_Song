946e686ae5e52c7e7aa8fbb81f4ffb7408c55ab5
daehyeon <daehyeon1999@naver.com>
2021-07-22 10:05:15 +0900

    change file names

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
-// const express = require('express');
-// const app = express();
-// const mongoose = require('mongoose');
-// const router = require('./routes/index') //routes
-
-// require('dotenv').config();
-
-// //connect to Mongodb Atlas
-// mongoose.connect(process.env.MONGO_URL,
-// {useNewUrlParser: true}).then(() => {
-//     console.log("Connected to Mongodb server");
-// })
-
-// //configure connection to MongoDb
-// const db = mongoose.connection;
-// db.on('error', console.error);
-
-// //middlewares
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-
-// //routes
-// app.use('/',router);
-
-// const port = process.env.port || 3000;
-
-
-// const server = app.listen(port,()=>{
-// console.log("Server is Running...");
-// });
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const router = require('./routes/index') //routes
+const router = require('./routes/router') //routes
 
 require('dotenv').config();
 
---
+++ routes/router.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const Data = require('../models/model');
+
+//post
+router.post('/new', async (req,res)=>{
+
+    const contact = new Data({
+        lname : req.body.lname,
+        fname : req.body.fname,
+        phone_number : req.body.phone_number
+    });
+
+    await contact.save().then(contact => {
+        res.send(contact);
+    }).catch(error => {
+        res.status(500).send("Fail to add");
+    })
+    res.end();
+
+})
+
+// Get all Data
+router.get('/', async (req,res)=>{
+    res.json(data = await Data.find());
+})
+
+//Get by Last name
+router.get('/getByLname',async(req,res)=>{
+    const data = await Data.find({lname:req.body.lname}).then(data => {
+        res.json(data);
+        });
+    //res.json(data = await Data.findOne({lname : req.params.lname}));
+})
+
+//Get by First name
+router.get('/getByFname',async(req,res)=>{
+    res.json(data = await Data.find({fname: req.body.fname}));
+})
+
+//Get by Phone Number
+router.get('/getByPhoneNumber', async(req,res)=>{
+    res.json(data = await Data.findOne({phone_number: req.body.phone_number}));
+})
+
+// Delete Data
+router.delete('/delete/:_id', async(req,res)=>{
+    const deleteData = await Data.findByIdAndDelete(req.params._id);
+    res.json(deleteData);
+})
+
+// Update
+router.patch('/update/:_id', async(req,res)=>{
+    const put = await Data.findByIdAndUpdate(req.params._id,
+        {
+            lname: req.body.lname,
+            fname: req.body.fname,
+            phone_number: req.body.phone_number
+        }
+        )
+    res.json(put);
+})
+
+module.exports = router;
